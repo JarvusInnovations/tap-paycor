0a62e526e6ed0d73ecb6804e26d8005f5206b9da
Alisa Aylward <alisaaylward@Alisas-MacBook-Pro.local>
2022-09-05 15:44:47 -0400

    add setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='tap-paycor',
+      version='0.0.1',
+      description='Singer.io tap for extracting data from Paycor',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['tap_paycor'],
+      install_requires=[
+          'singer-python==5.12.1',
+          'requests==2.20.0',
+          'backoff==1.8.0'
+      ],
+      entry_points='''
+          [console_scripts]
+          tap-paycor=tap_paycor:main
+      ''',
+      packages=['tap_paycor'],
+      package_data = {
+          'tap_paycor': ['tap_paycor/schemas/*.json']
+      },
+      include_package_data=True
+)
